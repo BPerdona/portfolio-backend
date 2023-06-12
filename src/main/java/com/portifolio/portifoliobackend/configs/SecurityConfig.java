30a9317635bd9617ff3a1dadea3564f66a0136fd
Bruno Perdona <bruno15perdona@gmail.com>
2023-06-11 22:23:09 -0300

    :bricks: ci: Adicionando diretórios básicos.

--- src/main/java/com/portifolio/portifoliobackend/configs/SecurityConfig.java
+++ src/main/java/com/portifolio/portifoliobackend/configs/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.portifolio.portifoliobackend.config;
+package com.portifolio.portifoliobackend.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
